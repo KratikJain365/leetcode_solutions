3f7e8aa9f717373c7ae6bd4354412f8297f6c6ed
KratikJain365 <2k24.csaiml1b.2411917@gmail.com>
2026-09-08 22:20:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<1000){
+            return 0;
+        }
+        else if(n<=100000){
+            int ans= (n-999);
+            return ans;
+
+        }
+        else{
+            return -1;
+        }
+        
+    }
+};
